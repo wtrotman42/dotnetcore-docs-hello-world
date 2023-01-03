c551963d514d7fc5ac2c4ba9ed58507e89874f9c
wtrotman42 <121896792+wtrotman42@users.noreply.github.com>
2023-01-03 13:33:12 -0600

    Update Privacy.cshtml.cs

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -6,14 +6,17 @@ namespace dotnetcoresample.Pages;
 public class PrivacyModel : PageModel
 {
     private readonly ILogger<PrivacyModel> _logger;
+    private readonly IConfiguration _config;
 
-    public PrivacyModel(ILogger<PrivacyModel> logger)
+    public PrivacyModel(ILogger<PrivacyModel> logger, IConfiguration config)
     {
         _logger = logger;
+        _config = config;
     }
 
     public void OnGet()
     {
+        ViewData["Name"] = _config["Name"];
     }
 }
 
